1e636ec939ec48dededeaaee83369176d45f5039
Mateusz Mikulski <mikulskee@gmail.com>
2021-03-04 12:09:07 +0100

    refactor Pagination.js

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Page from '../Page/Page';
 
-const Pagination = ({ currentPageNumber, totalNumberOfPages, onChange }) => {
-	const pages = Array.from(Array(totalNumberOfPages).keys()).map((pageNumber) => {
-		return (
-			<Page
-				key={pageNumber}
-				currentPageNumber={currentPageNumber}
-				pageNumber={pageNumber}
-				onChange={onChange}
-			/>
-		);
-	});
+const Pagination = (props) => {
+	const { currentPageNumber, totalNumberOfPages, onChange } = props;
 
-	return <ul className="pagination">{Boolean(pages.length) && pages}</ul>;
+	const [pagesButtons, setPagesButtons] = useState();
+
+	useEffect(() => {
+		const pages = Array.from(Array(totalNumberOfPages).keys()).map((pageNumber) => {
+			return (
+				<Page
+					key={pageNumber}
+					currentPageNumber={currentPageNumber}
+					pageNumber={pageNumber}
+					onChange={onChange}
+				/>
+			);
+		});
+		setPagesButtons(pages);
+	}, [currentPageNumber, totalNumberOfPages]);
+
+	return <ul className="pagination">{pagesButtons}</ul>;
 };
 
 export default Pagination;
