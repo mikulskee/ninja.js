38666b80a57a8aa7a52b3a05b35eef898fa83a70
Mateusz Mikulski <mikulskee@gmail.com>
2021-03-04 12:52:11 +0100

    Updates

---
+++ precache-manifest.ed473fac2a0faa2e75e0075d1632fd8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c64dd7bc3c9d529a2d4d7dbf3a1751",
+    "url": "/ninja.js/index.html"
+  },
+  {
+    "revision": "6e12446030ec30a2ec06",
+    "url": "/ninja.js/static/css/main.b8d8c66a.chunk.css"
+  },
+  {
+    "revision": "85eb91f91266ae56291a",
+    "url": "/ninja.js/static/js/2.da2d5981.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ninja.js/static/js/2.da2d5981.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e12446030ec30a2ec06",
+    "url": "/ninja.js/static/js/main.2bf590a6.chunk.js"
+  },
+  {
+    "revision": "bfb59f23b8f1365521b7",
+    "url": "/ninja.js/static/js/runtime-main.991455b8.js"
+  }
+]);
